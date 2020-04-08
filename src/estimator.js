ee270261bc0f5c5e7142d9759c4305d939917dc3
kkibet <kevin.kibet@ke.wananchi.com>
2020-04-08 23:12:07 +0300

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const periodToDays = (timeToElapse, periodType) => {
+  switch (periodType) {
+    case 'weeks':
+      return timeToElapse * 7;
+    case 'months':
+      return timeToElapse * 30;
+    default:
+      return timeToElapse;
+  }
+};
+
+const covid19ImpactEstimator = (data) => {
+  const output = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+  const days = periodToDays(data.timeToElapse, data.periodType);
+  output.impact.currentlyInfected = data.reportedCases * 10;
+  output.severeImpact.currentlyInfected = data.reportedCases * 50;
+  // eslint-disable-next-line max-len
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** (days / 3));
+  // eslint-disable-next-line max-len
+  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** (days / 3));
+  return output;
+};
 
 export default covid19ImpactEstimator;
